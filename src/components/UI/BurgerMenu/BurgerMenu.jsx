514526a5ff517f71fe17456c9b69d0db85f9d804
douryne <douryne.1st@gmail.com>
2022-04-18 16:59:39 +0500

    feature: CloseButton component

--- src/components/UI/BurgerMenu/BurgerMenu.jsx
+++ src/components/UI/BurgerMenu/BurgerMenu.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import { CloseButton } from '../CloseButton/CloseButton';
+
 import './BurgerMenu.css';
 
 export const BurgerMenu = ({ elements, visible, setVisible }) => {
   return (
     <div>
       <div className={visible ? 'nav_burger_menu active' : 'nav_burger_menu'}>
-        <button className='removeBtn' onClick={() => setVisible()} type='button'>
-          ✖
-        </button>
+        <CloseButton handleClose={setVisible} />
         <ul className='nav navbar-nav nav_links'>
           {elements.map((navLink) => (
             <li key={navLink.href} className='dropdown'>
