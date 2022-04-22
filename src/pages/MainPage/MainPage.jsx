9a6c78c15c6cc8ee93eef4fb3562565f8db59c83
Roman Musin <roman9m@yandex.ru>
2022-04-23 02:00:43 +0300

    make aboutProject and mainSlider components

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,17 @@
 import './MainPage.css';
 import React from 'react';
+import { SphereSection } from '../../components/UI/SphereSection/SphereSection';
 import { ProfEquipment } from '../../components/UI/ProfEquipment/ProfEquipment';
+import { AboutProject } from '../../components/UI/AboutProject/AboutProject';
+import { MainSlider } from '../../components/UI/MainSlider/MainSlider';
 
 export const MainPage = () => {
-  return <ProfEquipment />;
+  return (
+    <>
+      <MainSlider />
+      <SphereSection />
+      <ProfEquipment />
+      <AboutProject />
+    </>
+  );
 };
