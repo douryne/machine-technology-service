4aebbbd1296cd169f8ccf84fabfdb3e655396bfa
douryne <douryne.1st@gmail.com>
2022-04-25 21:51:17 +0500

    feature: fix style bug

--- src/components/UI/Footer/Footer.jsx
+++ src/components/UI/Footer/Footer.jsx
@@ -13,54 +13,56 @@ export const Footer = () => {
   ];
 
   return (
-    <footer className='footer'>
-      <div className='container'>
-        <div className='row'>
-          <div
-            className='col-lg-3 col-sm-3 address wow fadeInUp'
-            data-wow-duration='2s'
-            data-wow-delay='.1s'
-            style={{ width: '50%' }}
-          >
-            <h1>КОНТАКТЫ</h1>
-            <address>
-              <p>
-                <i className='fa fa-globe pr-10' /> Узбекистан, Ташкентская область,
-              </p>
-              <p>
-                <i className='fa fa-home pr-10' /> 110100 г. Алмалык пос. Бустон
-              </p>
-              <p>
-                <a href='tel:+998935006532' className='fa fa-mobile pr-10'>
-                  (998 93) 500 65 32
-                </a>
-              </p>
-              <p>
-                <a href='tel:+998946246875' className='fa fa-mobile pr-10'>
-                  (998 94) 624 68 75
-                </a>
-              </p>
-              <p>
-                <i className='fa fa-envelope pr-10' />
-                <a href='mailto:dilshod.h84@gmail.com'>dilshod.h84@gmail.com</a>
-              </p>
-            </address>
-          </div>
+    <div>
+      <footer className='footer'>
+        <div className='container'>
+          <div className='row'>
+            <div
+              className='col-lg-3 col-sm-3 address wow fadeInUp'
+              data-wow-duration='2s'
+              data-wow-delay='.1s'
+              style={{ width: '50%' }}
+            >
+              <h1>КОНТАКТЫ</h1>
+              <address>
+                <p>
+                  <i className='fa fa-globe pr-10' /> Узбекистан, Ташкентская область,
+                </p>
+                <p>
+                  <i className='fa fa-home pr-10' /> 110100 г. Алмалык пос. Бустон
+                </p>
+                <p>
+                  <a href='tel:+998935006532' className='fa fa-mobile pr-10'>
+                    (998 93) 500 65 32
+                  </a>
+                </p>
+                <p>
+                  <a href='tel:+998946246875' className='fa fa-mobile pr-10'>
+                    (998 94) 624 68 75
+                  </a>
+                </p>
+                <p>
+                  <i className='fa fa-envelope pr-10' />
+                  <a href='mailto:dilshod.h84@gmail.com'>dilshod.h84@gmail.com</a>
+                </p>
+              </address>
+            </div>
 
-          <div className='col-lg-3 col-sm-3'>
-            <div className='page-footer wow fadeInUp' data-wow-duration='2s' data-wow-delay='.5s'>
-              <h1>меню</h1>
-              <ul className='page-footer-list'>
-                {footerNav.map((navLink) => (
-                  <li key={navLink.title}>
-                    <Link to={navLink.link}>{navLink.title}</Link>
-                  </li>
-                ))}
-              </ul>
+            <div className='col-lg-3 col-sm-3'>
+              <div className='page-footer wow fadeInUp' data-wow-duration='2s' data-wow-delay='.5s'>
+                <h1>меню</h1>
+                <ul className='page-footer-list'>
+                  {footerNav.map((navLink) => (
+                    <li key={navLink.title}>
+                      <Link to={navLink.link}>{navLink.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
