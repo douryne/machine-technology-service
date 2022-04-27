7d4cf5c7d3a6fc149465ee8f77fc0e31d29c3a54
Aneckon <aneckogri@gmail.com>
2022-04-27 08:20:08 +0300

    Essays UI Component

---
+++ src/pages/MainEssays/MainEssays.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title, Table } from '../../components';
+
+import './MainEssays.css';
+
+export const MainEssays = () => {
+  return (
+    <div>
+      <Title>Наши референсы</Title>
+      <Table />
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { MainPage } from './MainPage/MainPage';
 export { MainPortfolio } from './MainPortfolio/MainPortfolio';
+export { MainEssays } from './MainEssays/MainEssays';
