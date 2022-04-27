80e1a3508d3c2f4cd56044c05ad229000547f2bc
douryne <douryne.1st@gmail.com>
2022-04-27 15:50:22 +0500

    fix: importance checking

--- src/components/UI/Texts/BlueTitle/BlueTitle.jsx
+++ src/components/UI/Texts/BlueTitle/BlueTitle.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import classes from './BlueTitle.module.css';
 
 export const BlueTitle = ({ children, importance }) => {
-  const HeaderTag = `h${importance}`;
+  const HeaderTag = `h${importance < 1 || importance > 6 ? 1 : importance}`;
   return <HeaderTag className={classes.title}>{children}</HeaderTag>;
 };
--- src/components/UI/Texts/GreyTitle/GreyTitle.jsx
+++ src/components/UI/Texts/GreyTitle/GreyTitle.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import classes from './GreyTitle.module.css';
 
 export const GreyTitle = ({ children, importance }) => {
-  const HeaderTag = `h${importance}`;
+  const HeaderTag = `h${importance < 1 || importance > 6 ? 1 : importance}`;
   return <HeaderTag className={classes.title}>{children}</HeaderTag>;
 };
