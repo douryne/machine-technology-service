f56d297bf45fd246e00ce2862e28f0c9a0403c53
douryne <douryne.1st@gmail.com>
2022-04-30 20:54:30 +0500

    refactor: About Project

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Panel, PanelGroup } from 'rsuite';
 import { useAnimations } from '../../hooks/useAnimations';
 import { aboutProjectInfo } from '../../utils/aboutProjectInfo';
-import { BlueTitle } from '..';
+import { Panels } from '..';
 
 import 'rsuite/dist/rsuite.min.css';
 import './AboutProject.css';
@@ -15,19 +14,7 @@ export const AboutProject = () => {
   return (
     <section data-aos='fade-up' className='aboutProject'>
       <h2 className='aboutProject__title'>О проекте</h2>
-      <PanelGroup className='aboutProject__panel-gruop' accordion>
-        {articles.length ? (
-          articles.map((article) => (
-            <div key={article.title} className='aboutProject__list-item'>
-              <Panel className='aboutProject__panel' header={article.title} shaded>
-                <p className='aboutProject__list-text'>{article.text}</p>
-              </Panel>
-            </div>
-          ))
-        ) : (
-          <BlueTitle importance={1}>Что-то пошло не так</BlueTitle>
-        )}
-      </PanelGroup>
+      <Panels headings={articles} />
     </section>
   );
 };
